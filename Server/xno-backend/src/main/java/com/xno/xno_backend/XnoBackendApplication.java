7fbe61983609c74ae53b992463f801d5628b45ae
Kevin Donovan <donovankevin58@gmail.com>
2026-02-09 18:56:34 -0500

    created Spring backend project

---
+++ Server/xno-backend/src/main/java/com/xno/xno_backend/XnoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.xno.xno_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XnoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XnoBackendApplication.class, args);
+	}
+
+}
---
+++ Server/xno-backend/src/test/java/com/xno/xno_backend/XnoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xno.xno_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XnoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
